bd25cf7bc7348ef2fdfbab8f985b3e07f4a56fcb
KRISHNA-ai-stack <sharmakrishna1142@gmail.com>
2025-12-26 18:09:56 +0530

    Added Emoji in Product.jsx

--- src/Product.jsx
+++ src/Product.jsx
@@ -2,7 +2,7 @@ import "./Product.css"
 function Product() {
     return (
         <div className="Product">
-            <h3>Product Title</h3>
+            <h3>Product Title 📱</h3>
             <h5>Product Description</h5>
         </div>
     )
